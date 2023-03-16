454cb7b23ac6a27205d7afda93ab67a3a659f9fe
petrosAth <develop@athanasoulis.me>
2023-03-16 16:14:54 +0200

    Add main module

--- src/modules/pageLoad.js
+++ src/modules/pageLoad.js
@@ -1,9 +1,11 @@
 import header from './pages/header';
 import nav from './pages/nav';
+import main from './pages/main';
 
 const pageLoad = () => {
   header();
   nav();
+  main();
 };
 
 export default pageLoad;
