fae16a33e3e3d3c3700758d8588618571bd25510
petrosAth <develop@athanasoulis.me>
2023-03-17 11:47:24 +0200

    Add mainCuntactUs module

---
+++ src/modules/pages/mainContactUs.js
@@ -0,0 +1,13 @@
+import * as utils from '../utilities';
+
+const mainContactUs = () => {
+  const menu = utils.createNewElement(
+    'main',
+    'content__contact-us',
+    'Lorem ipsum dolor sit amet, . Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.'
+  );
+
+  utils.appendChildren([[document.body, menu]]);
+};
+
+export default mainContactUs;
