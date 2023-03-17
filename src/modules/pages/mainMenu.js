6cf151a655f8c8dd44c6037d2e3a2071a36c90dd
petrosAth <develop@athanasoulis.me>
2023-03-17 11:47:17 +0200

    Add mainMenu module

---
+++ src/modules/pages/mainMenu.js
@@ -0,0 +1,13 @@
+import * as utils from '../utilities';
+
+const mainMenu = () => {
+  const menu = utils.createNewElement(
+    'main',
+    'content__menu',
+    'Lorem ipsum dolor sit amet, anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.'
+  );
+
+  utils.appendChildren([[document.body, menu]]);
+};
+
+export default mainMenu;
