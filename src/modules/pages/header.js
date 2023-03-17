6e6d65855f59c53b3c05065690b4b3baf9212f5e
petrosAth <develop@athanasoulis.me>
2023-03-17 14:20:44 +0200

    Add logo image through SCSS instead of JS

--- src/modules/pages/header.js
+++ src/modules/pages/header.js
@@ -1,13 +1,10 @@
 import * as utils from '../utilities';
-import logoSvg from '../../assets/images/logo.svg';
 
 const header = () => {
   const header = utils.createNewElement('header', 'header');
   const divLogo = utils.createNewElement('div', 'logo');
-  const imgLogo = utils.createNewElement('img', 'logo__img', logoSvg);
 
   utils.appendChildren([
-    [divLogo, imgLogo],
     [header, divLogo],
     [document.body, header],
   ]);
