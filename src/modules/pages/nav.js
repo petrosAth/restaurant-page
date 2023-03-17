dd6cf71f536de121f137175310bd33bcae8db3f9
petrosAth <develop@athanasoulis.me>
2023-03-17 12:31:07 +0200

    Change CSS class name for nav menu

--- src/modules/pages/nav.js
+++ src/modules/pages/nav.js
@@ -1,14 +1,14 @@
 import * as utils from '../utilities';
 
 const nav = () => {
-  const nav = utils.createNewElement('nav', 'menu');
-  const ul = utils.createNewElement('ul', 'menu__container');
-  const liHome = utils.createNewElement('li', 'menu__option');
-  const btnHome = utils.createNewElement('button', ['button', 'menu__button__home'], 'home');
-  const liMenu = utils.createNewElement('li', 'menu__option');
-  const btnMenu = utils.createNewElement('button', ['button', 'menu__button__menu'], 'menu');
-  const liContact = utils.createNewElement('li', 'menu__option');
-  const btnContact = utils.createNewElement('button', ['button', 'menu__button__contact'], 'contact us');
+  const nav = utils.createNewElement('nav', 'nav-menu');
+  const ul = utils.createNewElement('ul', 'nav-menu__container');
+  const liHome = utils.createNewElement('li', 'nav-menu__option');
+  const btnHome = utils.createNewElement('button', ['button', 'nav-menu__button__home'], 'home');
+  const liMenu = utils.createNewElement('li', 'nav-menu__option');
+  const btnMenu = utils.createNewElement('button', ['button', 'nav-menu__button__menu'], 'menu');
+  const liContact = utils.createNewElement('li', 'nav-menu__option');
+  const btnContact = utils.createNewElement('button', ['button', 'nav-menu__button__contact'], 'contact us');
 
   utils.appendChildren([
     [liHome, btnHome],
