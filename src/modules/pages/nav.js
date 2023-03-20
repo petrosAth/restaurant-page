c64943dd6cfa5415ebffbfcf54f06832e17f0d97
petrosAth <develop@athanasoulis.me>
2023-03-20 07:34:30 +0200

    Adapt local buttons styling

--- src/modules/pages/nav.js
+++ src/modules/pages/nav.js
@@ -4,11 +4,11 @@ const nav = () => {
   const nav = utils.createNewElement('nav', 'nav-menu');
   const ul = utils.createNewElement('ul', 'nav-menu__container');
   const liHome = utils.createNewElement('li', 'nav-menu__option');
-  const btnHome = utils.createNewElement('button', ['button', 'nav-menu__button__home'], 'home');
+  const btnHome = utils.createNewElement('button', ['button', 'nav-menu__button'], 'home');
   const liMenu = utils.createNewElement('li', 'nav-menu__option');
-  const btnMenu = utils.createNewElement('button', ['button', 'nav-menu__button__menu'], 'menu');
+  const btnMenu = utils.createNewElement('button', ['button', 'nav-menu__button'], 'menu');
   const liContact = utils.createNewElement('li', 'nav-menu__option');
-  const btnContact = utils.createNewElement('button', ['button', 'nav-menu__button__contact'], 'contact us');
+  const btnContact = utils.createNewElement('button', ['button', 'nav-menu__button'], 'contact us');
 
   utils.appendChildren([
     [liHome, btnHome],
