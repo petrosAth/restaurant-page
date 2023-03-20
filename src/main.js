2bb69edbbe2f091fd2648595ad4148384b8bca9b
petrosAth <develop@athanasoulis.me>
2023-03-20 07:54:22 +0200

    Add menu functionality

--- src/main.js
+++ src/main.js
@@ -2,3 +2,20 @@ import './stylesheets/main.scss';
 import pageLoad from './modules/pageLoad';
 
 const load = pageLoad;
+
+(() => {
+  const menuButtons = {
+    btnHome: '.nav-menu__button__home',
+    btnMenu: '.nav-menu__button__menu',
+    btnContact: '.nav-menu__button__contactUs',
+  };
+
+  Object.keys(menuButtons).forEach((button) => {
+    const btn = menuButtons[button];
+    document.querySelector(btn).addEventListener('click', () => {
+      load(btn.replace('.nav-menu__button__', ''));
+    });
+  });
+
+  return;
+})();
--- src/modules/pages/nav.js
+++ src/modules/pages/nav.js
@@ -4,11 +4,15 @@ const nav = () => {
   const nav = utils.createNewElement('nav', 'nav-menu');
   const ul = utils.createNewElement('ul', 'nav-menu__container');
   const liHome = utils.createNewElement('li', 'nav-menu__option');
-  const btnHome = utils.createNewElement('button', ['button', 'nav-menu__button'], 'home');
+  const btnHome = utils.createNewElement('button', ['button', 'nav-menu__button', 'nav-menu__button__home'], 'home');
   const liMenu = utils.createNewElement('li', 'nav-menu__option');
-  const btnMenu = utils.createNewElement('button', ['button', 'nav-menu__button'], 'menu');
+  const btnMenu = utils.createNewElement('button', ['button', 'nav-menu__button', 'nav-menu__button__menu'], 'menu');
   const liContact = utils.createNewElement('li', 'nav-menu__option');
-  const btnContact = utils.createNewElement('button', ['button', 'nav-menu__button'], 'contact us');
+  const btnContact = utils.createNewElement(
+    'button',
+    ['button', 'nav-menu__button', 'nav-menu__button__contactUs'],
+    'contact us'
+  );
 
   utils.appendChildren([
     [liHome, btnHome],
